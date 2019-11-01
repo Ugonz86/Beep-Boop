32a6d70901a5877c4459c4504b351d9c805bb17c
Ugonz86 <ugonzalez86@gmail.com>
2019-11-01 16:17:51 -0700

    Add js fix. The program returns the specifications in console log. Next step is to append results in the html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
-var userInput = 5;
-
-
 $(document).ready(function() {
 
   $("form#beepBoopGenerator").submit(function(event) {
     event.preventDefault();
-    var numbers = [];
+    var userInput = $("#numberInput").val();
+    var beepBoop = [];
       for (var i = 0; i <= userInput; i++) {
-        numbers[i] = i;
+        if (i.toString().indexOf('3') > -1) {
+          beepBoop[i] = "I'm afraid I can't do that. Try again!";
+        }
+        else if (i.toString().indexOf('2') > -1) {
+          beepBoop[i] = "Boop";
+        }
+        else if (i.toString().indexOf('1') > -1) {
+          beepBoop[i] = "Beep";
+        }
+        else {
+          beepBoop[i] = i;
+        }
       }
-      console.log(numbers);
+
+      console.log(beepBoop);
 
       $("#beepBoop").hide();
-      $(".result").text($("#nameInput").val() + ", I'm afraid I can't do that. Try again!");
+      $(".result").text($("#nameInput").val());
       $(".result").show();
 
   });
