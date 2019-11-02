eb7505c04e39158c532bc5b38066fb68c8b983d9
Ugonz86 <ugonzalez86@gmail.com>
2019-11-01 17:00:50 -0700

    Add comments to follow up with progress.

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,39 @@ $(document).ready(function() {
 
   $("form#beepBoopGenerator").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#numberInput").val();
+    var name = $("#nameInput").val(); //The user input.
+    var userInput = $("#numberInput").val(); //The number the user inputs in the UI form.
+
+//
     var beepBoop = [];
       for (var i = 0; i <= userInput; i++) {
         if (i.toString().indexOf('3') > -1) {
-          beepBoop[i] = "I'm afraid I can't do that. Try again!";
+          $("#list").append("<li>I'm afraid I can't do that. Try again!</li>");
+          // beepBoop[i] = "I'm afraid I can't do that. Try again!";
         }
         else if (i.toString().indexOf('2') > -1) {
-          beepBoop[i] = "Boop";
+          $("#list").append("<li>Boop!</li>");
+          // beepBoop[i] = "Boop";
         }
         else if (i.toString().indexOf('1') > -1) {
-          beepBoop[i] = "Beep";
+          $("#list").append("<li>Beep!</li>");
+          // beepBoop[i] = "Beep";
         }
         else {
-          beepBoop[i] = i;
+          $("#list").append("<li> " + i + " </li>");
+          // beepBoop[i] = i;
         }
       }
+      // console.log(beepBoop); //testing, testing.
 
-      console.log(beepBoop);
-
+      //When the user submits a number, the following hides the div where the user form is located and then it shows the result div.
       $("#beepBoop").hide();
-      $(".result").text($("#nameInput").val());
+      // $(".result").text($("#nameInput").val());
+      $("#nameInput").text(name);
       $(".result").show();
-
   });
+
+  //This function takes the user name which will be display with the results for UX personalization. Also hides its div and then shows the next one.
   $("#nameButton").click(function() {
     $("#nameDiv").hide("fadeOut");
     $("#beepBoop").fadeIn("slow");
